e4ea18f986c3e9132603922de2e65358824a96fb
artemiygusev04 <PMPO1002@yandex.ru>
2022-06-27 00:42:00 +0300

    test5

--- conftest.py
+++ conftest.py
@@ -4,7 +4,6 @@ from selene.support.shared import browser
 
 @pytest.fixture(scope='function', autouse=True)
 def dr_init():
-    browser.open('https://demoqa.com/automation-practice-form')
-    browser.config.driver.set_window_size(1920, 1080)
+    browser.config.base_url = 'https://demoqa.com'
     yield
     browser.quit()
--- test_form.py
+++ test_form.py
@@ -1,9 +1,16 @@
 from selene import be
 from selene.support.conditions import have
-from selene.support.jquery_style_selectors import s, ss
+from selene.support.shared import browser
+from selene.support.shared.jquery_style import s, ss
+
+
+def url_open_size(url, width=1920, height=1080):
+    browser.open(url)
+    browser.config.driver.set_window_size(width, height)
 
 
 def test_case1():
+    url_open_size('/automation-practice-form')
 
     # Заполняем форму
     s('#firstName').type('Jack')
@@ -20,20 +27,18 @@ def test_case1():
 
     s('#react-select-2-option-0').click()
 
-    s('#dateOfBirthInput').click()
-    s('[aria-label*="20th"]').click()
+    browser.execute_script('document.querySelector("#dateOfBirthInput").value = "27 Jun 2022";')
 
     s('[for=gender-radio-1]').click()
 
     s('[for=hobbies-checkbox-1]').click()
 
-    #select state
     s('#state').click()
     s('#react-select-3-option-0').click()
-    #select city
+
     s('#city').click()
     s('#react-select-4-option-0').click()
-    # Отправка картинки
+    # Либо: s('#uploadPicture').type(os.path.abspath('123.png')), но будет импорт 'os'
     s('#uploadPicture').type('C:\\123.png')
     s('#submit').click()
 
@@ -48,7 +53,7 @@ def test_case1():
                                       'Mobile',
                                       '4815162342',
                                       'Date of Birth',
-                                      '20 June,2022',
+                                      '27 June,2022',
                                       'Subjects',
                                       'Hindi',
                                       'Hobbies',
